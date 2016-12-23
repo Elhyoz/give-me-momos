156e25f9a9f395e5a520d5d19894f17576ac0ff0
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-23 12:40:12 -0600

    Reparada la galeria de momos. ya se muestra la descripcion

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Momo(models.Model):
     name = models.CharField(max_length=80, default='')
     description = models.CharField(max_length=150, default='')
-    image = models.ImageField(blank=False, upload_to='', default='')
+    image = models.ImageField(blank=False, upload_to='media/momos', default='')
--- momos/settings.py
+++ momos/settings.py
@@ -110,5 +110,5 @@ STATICFILES_DIRS = [
 
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = ''
